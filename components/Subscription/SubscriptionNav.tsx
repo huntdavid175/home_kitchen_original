6e8dfdc34279affcb35f1d6643093ad857aef791
huntdavid175 <huntdavid175@gmail.com>
2025-02-13 18:15:57 -0400

    temporary logo added

--- components/Subscription/SubscriptionNav.tsx
+++ components/Subscription/SubscriptionNav.tsx
@@ -70,7 +70,38 @@ export default function SubscriptionNav() {
       <div className="container mx-auto px-4 lg:px-6">
         {/* Progress Steps */}
         <div className="flex items-center justify-between py-4 border-b">
-          <div className="flex items-center gap-4 sm:gap-8 flex-1">
+          <div>
+            <svg
+              id="logo-15"
+              width="49"
+              height="48"
+              viewBox="0 0 49 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z"
+                className="ccustom"
+                fill="#17CF97"
+              ></path>
+              <path
+                d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z"
+                className="ccustom"
+                fill="#17CF97"
+              ></path>
+              <path
+                d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z"
+                className="ccustom"
+                fill="#17CF97"
+              ></path>
+              <path
+                d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z"
+                className="ccustom"
+                fill="#17CF97"
+              ></path>
+            </svg>
+          </div>
+          <div className="flex items-center gap-4 sm:gap-8 ">
             <div className="flex items-center gap-2">
               <div className="w-6 h-6 rounded-full bg-[#14b8a6] text-white flex items-center justify-center text-sm font-medium">
                 1
@@ -118,7 +149,7 @@ export default function SubscriptionNav() {
               <Badge
                 key={category.name}
                 className={cn(
-                  "rounded-full px-4 py-1.5 text-sm cursor-pointer transition-colors select-none",
+                  "rounded-full px-4 py-1.5 text-xs cursor-pointer transition-colors select-none",
                   activeCategories.includes(category.name)
                     ? "bg-[#14b8a6] hover:bg-[#0d9488] text-white"
                     : "bg-[#e2e8f0] text-[#0f172a] hover:bg-[#cbd5e1]"
