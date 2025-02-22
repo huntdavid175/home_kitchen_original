d79942e1708e10e6b3559b73b777987f9eeefa92
Immanuel Phillips <immanuelphillips022@gmail.com>
2025-02-22 14:44:27 +0000

    login

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import AuthForm from "@/components/Login/SignIn";
+import React from "react";
+
+export default function SignIn() {
+  
+    return (
+      <div className="">
+        <AuthForm />
+      </div>
+    );
+  }
