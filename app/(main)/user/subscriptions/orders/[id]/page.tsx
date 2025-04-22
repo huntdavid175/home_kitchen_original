45bc9910ccd1ba43deee35a84dbc869aaf51b2c3
huntdavid175 <huntdavid175@gmail.com>
2025-04-22 20:03:05 -0300

    build error fixed

--- app/(main)/user/subscriptions/orders/[id]/page.tsx
+++ app/(main)/user/subscriptions/orders/[id]/page.tsx
@@ -1,13 +1,11 @@
 import { DashboardTabs } from "@/components/User/Dashboard/DashBoardTabs";
 import { OrderDetails } from "@/components/User/Dashboard/OrderDetails";
-import { Metadata } from "next";
 
-type Props = {
+export default async function OrderDetailsPage({
+  params,
+}: {
   params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default function OrderDetailsPage({ params }: Props) {
+}) {
   return (
     <DashboardTabs defaultTab="orders">
       <OrderDetails orderId={params.id} />
